d2ec0c2d6bea4bfc9de0b56acb108537ddd9051c
Brandon <brandon.s.mckinley@gmail.com>
2024-03-16 20:54:58 -0700

    layout of functions and testing of armadillo library

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <initializer_list>
+#include <iostream>
+#include <cmath>
+#include <armadillo>
+
+// Initialize variables and modules
+//Initalize_EMG_Signal_Processing_Module()
+
+//Initalize_Gesture_Regcognition_Modle()
+
+//Initalize_Virtual_Prosthetic_Limb()
+
+//Initialize_Simulation_Environment()
+
+// Main loop to continoulsy read user input
+// Capture simulated EMG signals or gestures from user input
+//emg_signals = Capture_EMG_Signals_From_User()
+//recoginized_gesture = Recoginize_Gesture_From_User_Input()
+
+// Process EMG signals and recoginze gestures
+//processed_signals = Process_EMG_Signals(emg_signals)
+//limb_command = Translate_Gensture_To_Limb_Command(recognized_gesture)
+
+// Control virtual prosthetic limb
+//Move_Virtual_Limb(limb_commands)
+
+// Update simulation environtment and display
+//Update_Simulation_Envrionment()
+//Display_Virtual_Limb()
+
+// Check for user input to exit
+//if (User_PResses_Exit_Button())
+ //   simulation_running = false;
+
+//int main () { (THIS WAS TO TEST THE ARMADILLO LIBRARY)
+
+//  arma::vec v = {1, 2, 3};
+// std::cout << "vector v:\n" << v << std::endl;
+
+//return 0;
+//}
+
+
+
